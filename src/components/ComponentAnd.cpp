f08366cab0628c0f1a468743ac1de7bd7c842367
arthur <arthur.teisseire@epitech.eu>
2019-01-30 13:50:21 +0100

    Add empty implementation for dump and setLink in ComponentAnd

--- src/components/ComponentAnd.cpp
+++ src/components/ComponentAnd.cpp
@@ -7,12 +7,20 @@
 
 #include "ComponentAnd.hpp"
 
+nts::ComponentAnd::ComponentAnd(const std::string &name) :
+	AComponent(name)
+{
+}
+
+nts::Tristate nts::ComponentAnd::compute(size_t pin)
+{
+}
+
 void nts::ComponentAnd::setLink(size_t pin, nts::IComponent &other, size_t otherPin)
 {
 	_pins[pin] = other[otherPin];
 }
 
-nts::ComponentAnd::ComponentAnd(const std::string &name) :
-	AComponent(name)
+void nts::ComponentAnd::dump()
 {
 }
