ca22d3a52690502a8e7d251ac3ce28d9efa91f15
Adrien Fabre <adrien.fabre@epitech.eu>
2019-01-31 19:00:33 +0100

    init circuit parser classes

---
+++ src/CircuitParser.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_nanotekspice_2018
+** File description:
+** Created by Adrien FABRE,
+*/
+
+#include "CircuitParser.hpp"
---
+++ src/CircuitParser.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_nanotekspice_2018
+** File description:
+** Created by Adrien FABRE,
+*/
+
+#ifndef CPP_NANOTEKSPICE_2018_CIRCUITPARSER_HPP
+#define CPP_NANOTEKSPICE_2018_CIRCUITPARSER_HPP
+
+class CircuitParser {
+};
+
+#endif //CPP_NANOTEKSPICE_2018_CIRCUITPARSER_HPP
