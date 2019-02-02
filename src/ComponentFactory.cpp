c60c1d9ab0609df7ac60d3dd8c5c7bbcf55342a3
arthur <arthur.teisseire@epitech.eu>
2019-02-02 15:13:38 +0100

    Throw when Component is unkown and add output and And Component to Factory

--- src/ComponentFactory.cpp
+++ src/ComponentFactory.cpp
@@ -5,15 +5,22 @@
 ** ComponentFactory.cpp
 */
 
+#include "ParserException.hpp"
 #include "ComponentFactory.hpp"
 #include "ComponentInput.hpp"
+#include "ComponentOutput.hpp"
+#include "ComponentAnd.hpp"
 
 nts::ComponentFactory::ComponentTypes nts::ComponentFactory::_componentTypes = {
 	{"input", [](const std::string &value) { return std::make_unique<nts::ComponentInput>(value); }},
+	{"output", [](const std::string &value) { return std::make_unique<nts::ComponentOutput>(value); }},
+	{"and", [](const std::string &value) { return std::make_unique<nts::ComponentAnd>(value); }},
 };
 
 std::unique_ptr<nts::IComponent> nts::ComponentFactory::createComponent(const std::string &type,
                                                                          const std::string &value)
 {
+	if (_componentTypes.find(type) == _componentTypes.end())
+		throw ParserException("Component '" + type + "' doesn't exists");
 	return _componentTypes[type](value);
 }
