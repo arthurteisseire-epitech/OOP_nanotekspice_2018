fd22c74e33ddf61dfd08f775fff63d1377be3703
Arthamios <adrien.fabre@epitech.eu>
2019-02-07 01:15:15 +0100

    Format component tests: they now have ini, local compute, compute and parsing tests

--- tests/src/components/testsComponentAnd.cpp
+++ tests/src/components/testsComponentAnd.cpp
@@ -6,6 +6,8 @@
 */
 
 #include "gtest/gtest.h"
+#include "ComponentInput.hpp"
+#include "ComponentOutput.hpp"
 #include "ComponentAnd.hpp"
 
 TEST(ComponentAnd, init)
@@ -22,27 +24,55 @@ TEST(ComponentAnd, init)
 	EXPECT_EQ(andComp.getPin(2)->getState(), nts::UNDEFINED);
 }
 
-static void testPinState(nts::Tristate in1State, nts::Tristate in2State)
+static void testLocalCompute(nts::Tristate in1State, nts::Tristate in2State, nts::Tristate expectedOutputState)
 {
 	nts::ComponentAnd andComp("and");
 
 	andComp.getPin(0)->setState(in1State);
 	andComp.getPin(1)->setState(in2State);
 	andComp.localCompute();
-	EXPECT_EQ(andComp.getPin(0)->getState(), in1State);
-	EXPECT_EQ(andComp.getPin(1)->getState(), in2State);
-	EXPECT_EQ(andComp.getPin(2)->getState(), in2State && in1State);
+	EXPECT_EQ(andComp.getPin(2)->getState(), expectedOutputState);
 }
 
-TEST(ComponentAnd, pinState)
+TEST(ComponentAnd, localCompute)
 {
-	testPinState(nts::TRUE, nts::TRUE);
-	testPinState(nts::TRUE, nts::FALSE);
-	testPinState(nts::FALSE, nts::TRUE);
-	testPinState(nts::FALSE, nts::FALSE);
-	testPinState(nts::TRUE, nts::UNDEFINED);
-	testPinState(nts::UNDEFINED, nts::TRUE);
-	testPinState(nts::FALSE, nts::UNDEFINED);
-	testPinState(nts::UNDEFINED, nts::FALSE);
-	testPinState(nts::UNDEFINED, nts::UNDEFINED);
+	testLocalCompute(nts::TRUE, nts::TRUE, nts::TRUE);
+	testLocalCompute(nts::TRUE, nts::FALSE, nts::FALSE);
+	testLocalCompute(nts::FALSE, nts::TRUE, nts::FALSE);
+	testLocalCompute(nts::FALSE, nts::FALSE, nts::FALSE);
+	testLocalCompute(nts::TRUE, nts::UNDEFINED, nts::UNDEFINED);
+	testLocalCompute(nts::UNDEFINED, nts::TRUE, nts::UNDEFINED);
+	testLocalCompute(nts::FALSE, nts::UNDEFINED, nts::FALSE);
+	testLocalCompute(nts::UNDEFINED, nts::FALSE, nts::FALSE);
+	testLocalCompute(nts::UNDEFINED, nts::UNDEFINED, nts::UNDEFINED);
+}
+
+static void testCompute(nts::Tristate in1State, nts::Tristate in2State, nts::Tristate expectedOutputState)
+{
+	nts::ComponentInput in1("in1");
+	nts::ComponentInput in2("in2");
+	nts::ComponentOutput out("out");
+	nts::ComponentAnd andComp("and");
+
+	out.setLink(0, andComp, 2);
+	andComp.setLink(0, in1, 0);
+	andComp.setLink(1, in2, 0);
+
+	in1.getPin(0)->setState(in1State);
+	in2.getPin(0)->setState(in2State);
+	out.compute(0);
+	EXPECT_EQ(out.getPin(0)->getState(), expectedOutputState);
+}
+
+TEST(AndCompute, compute)
+{
+	testCompute(nts::TRUE, nts::TRUE, nts::TRUE);
+	testCompute(nts::TRUE, nts::FALSE, nts::FALSE);
+	testCompute(nts::FALSE, nts::TRUE, nts::FALSE);
+	testCompute(nts::FALSE, nts::FALSE, nts::FALSE);
+	testCompute(nts::TRUE, nts::UNDEFINED, nts::UNDEFINED);
+	testCompute(nts::UNDEFINED, nts::TRUE, nts::UNDEFINED);
+	testCompute(nts::FALSE, nts::UNDEFINED, nts::FALSE);
+	testCompute(nts::UNDEFINED, nts::FALSE, nts::FALSE);
+	testCompute(nts::UNDEFINED, nts::UNDEFINED, nts::UNDEFINED);
 }
