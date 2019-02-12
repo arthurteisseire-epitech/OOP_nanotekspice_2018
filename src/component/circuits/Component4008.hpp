c6235e38cdd8054b93c62eb24d58295e75a77448
Arthamios <adrien.fabre@epitech.eu>
2019-02-13 00:35:31 +0100

    Add Component4008 (adder) without CarryLookAheadGenerator

---
+++ src/component/circuits/Component4008.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2018
+** OOP_nanotekspice_2018
+** File description:
+** Component4008.hpp,
+*/
+
+#ifndef OOP_NANOTEKSPICE_2018_COMPONENT4008_HPP
+#define OOP_NANOTEKSPICE_2018_COMPONENT4008_HPP
+
+#include "ACircuit.hpp"
+
+namespace nts {
+	class Component4008 : public ACircuit {
+	public:
+		explicit Component4008(const std::string &name);
+	};
+}
+
+#endif //OOP_NANOTEKSPICE_2018_COMPONENT4008_HPP
